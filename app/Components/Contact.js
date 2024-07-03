57bbcd1a60fca0a93aa5a907b97e80e156a9e52a
nilesh7757 <nileshmori7757@gmail.com>
2024-07-03 21:07:57 +0530

    contact page added

--- app/Components/Contact.js
+++ app/Components/Contact.js
@@ -1,13 +1,139 @@
-import React from 'react'
-
+"use client";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import { motion } from "framer-motion";
+import { IoIosContact, IoIosSend } from "react-icons/io";
+import { IoPerson } from "react-icons/io5";
+import { MdMail, MdPhone } from "react-icons/md";
+import { FaMessage } from "react-icons/fa6";
+// import { IoIosSend } from "react-icons/io";
+import Image from "next/image";
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+    emailjs
+      .sendForm("service_2qycq85", "template_ebfrijf", form.current, {
+        publicKey: "lgyXGdU7WDqjRFTGd",
+      }) // Replace with your actual values
+      .then(
+        (response) => {
+          console.log("SUCCESS!", response.status, response.text);
+          alert("Comment Sent Successfully!"); // User-friendly success message
+          e.target.reset(); // Reset form fields
+        },
+        (error) => {
+          console.error("FAILED...", error);
+          alert("Comment Sending Failed. Please try again."); // User-friendly error message
+        }
+      );
+  };
+
   return (
     <>
-    <div id='contact' className='py-6'>
-        <h3 className='text-3xl text-center'>Contact Me</h3>
-    </div>
-    </>
-  )
-}
+      <div className="w-full py-6 flex h-[70%]  flex-col items-center">
+      <h2 className="md:text-4xl text-3xl font-bold text-center flex gap-2"><IoPerson/> Contact <span className="bg-gradient-to-r from-sky-500 to-sky-200 ... bg-clip-text text-transparent">Me</span></h2>
 
-export default Contact
+        <div className="flex h-[90%] mt-6 w-full md:w-[80%]">
+        <Image 
+            className="w-full md:block  hidden rounded-md md:w-[40%]"
+            src={"./Contact.png"}
+            width={20}
+            height={250}
+          />
+        {/* <div className="w-[30%] h-[100%]">
+        <Image
+        className="absolute z-0 w-full h-full object-cover rounded-md"
+        src={"./Contact.png"}
+        layout="fill"
+        objectFit="contain"
+      />
+        </div> */}
+        <form
+          className="flex-col  justify-center items-center w-full  md:w-[60%]"
+          ref={form}
+          onSubmit={sendEmail}
+        >
+    <div className="flex flex-col gap-5 justify-center w-full h-full ">
+    <div className="relative  mx-auto flex w-[80%] items-center">
+    <IoIosContact className="text-gray-500 absolute left-3" />
+    <input
+      type="text"
+      name="user_name"
+      placeholder="Name"
+      className="pl-10 p-2 border border-blue-500 rounded-lg w-full"
+      required
+    />
+  </div>
+    <div className="relative  mx-auto flex w-[80%] items-center">
+    <MdMail className="text-gray-500 absolute left-3" />
+    <input
+      type="email"
+      name="user_email"
+      placeholder="Email"
+      className="pl-10 p-2 border border-blue-500 rounded-lg w-full"
+      required
+    />
+  </div>
+    <div className="relative  mx-auto flex w-[80%] items-center">
+    <MdPhone className="text-gray-500 absolute left-3" />
+    <input
+      type="number"
+      name="mobile_number"
+      placeholder="Mobile Number"
+      className="pl-10 p-2 border border-blue-500 rounded-lg w-full"
+      required
+    />
+  </div>
+    <div className="relative  mx-auto flex w-[80%]">
+    <FaMessage className="text-gray-500 absolute mt-3 left-3" />
+    {/* <textarea
+      type="email"
+      name="user_email"
+      placeholder="Message"
+      rows={40}
+      cols={40}
+      className="pl-10 p-2 border h-48 border-blue-500 rounded-lg w-full"
+      required
+    >
+      Hi
+    </textarea> */}
+     <textarea
+              className="pl-10 p-2 border border-blue-500 rounded-lg w-full"
+              name="message"
+              id=""
+              cols="60"
+              rows="5"
+              placeholder="Message"
+            />
+  </div>
+  <div className="w-full flex justify-center mt-6 lg:mt-2">
+              <motion.button
+                whileInView={{ opacity: 1, y: 0 }}
+                whileTap={{scale:1.05,transition: { duration: 0.18, ease: "easeOut" }}}
+                initial={{
+                  y: -50,
+                  opacity: 0,
+                }}
+                transition={{
+                  duration: 0.5,
+                }}
+                whileHover={{
+                  scale: 1.05,
+                }}
+                className="bg-black hover:bg-white hover:text-black px-2 py-2 border border-gray-400 w-[20%] text-white font-semibold rounded-lg flex items-center justify-center gap-2 mx-auto"
+              >
+                Submit
+                <IoIosSend />
+              </motion.button>
+            </div>
+    </div>
+        </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
