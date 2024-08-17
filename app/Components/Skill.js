f0d432e8c5dae6304c399e0409275d665e464414
nilesh7757 <nileshmori7757@gmail.com>
2024-08-17 16:11:09 +0530

    site connection add in sliders

--- app/Components/Skill.js
+++ app/Components/Skill.js
@@ -1,6 +1,5 @@
 'use client'
 import React, { useRef, useState } from 'react';
-// Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 // import IconC from 'react-devicon/c/original' 
 import 'swiper/css';
@@ -11,19 +10,16 @@ import 'swiper/css/navigation';
 import 'devicon/devicon.min.css';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation'
 
 // import required modules
 //  import IconC from 'react-devicon/c/original' 
 import { Autoplay,EffectCoverflow,Navigation,Pagination } from 'swiper/modules';
-import { FaReact,FaFigma, FaCss3Alt, FaHtml5,FaGithub, FaJava, FaAndroid,FaLaptopCode } from 'react-icons/fa';
-import { IoLogoJavascript } from 'react-icons/io5';
-import { RiNextjsFill} from "react-icons/ri";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { SiC, SiCplusplus,SiCsharp } from 'react-icons/si';
-import { FaPython } from "react-icons/fa";
-import { SiKotlin } from "react-icons/si";
+import { FaAndroid,FaLaptopCode } from 'react-icons/fa';
+import { Link } from 'react-scroll';
 // import IconC from 'react-devicon/c/original'
 export default function Skill() {
+  const router = useRouter()
   return (
     <>
       <div  className='py-6'>
@@ -62,62 +58,62 @@ export default function Skill() {
         className="mySwiper"
       >
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://developer.android.com/")} className='text-center flex flex-col items-center'>
               <FaAndroid className='md:size-[200px] size-[150px] ' fill='#3DDC84'/>
               <span className='mt-1'>Android</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://cloud.mongodb.com/v2/666c7c309d186e472043683b#/overview")} className='text-center flex flex-col items-center'>
             <Image alt=""  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mongodb/mongodb-original.svg" width={200} height={200}/>
 
               <span className='mt-1'>MongoDB</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://www.mysql.com/")} className='text-center flex flex-col items-center'>
             <Image alt=""  src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/mysql/mysql-original-wordmark.svg" width={200} height={200}/>
 
               <span className='mt-1'>My SQL</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://react.dev/")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/react/react-original.svg" width={200} height={200}/>
 
               <span className='mt-1'>React</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://nextjs.org/")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nextjs/nextjs-original.svg" width={200} height={200}/>
 
               <span className='mt-1'>Next</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://nodejs.org/en")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/nodejs/nodejs-original-wordmark.svg" width={200} height={200}/>
 
               <span className='mt-1'>Node</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://tailwindcss.com/")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg" width={200} height={200}/>
 
               <span className='mt-1'>Tailwind Css</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://expressjs.com/")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/express/express-original.svg" width={200} height={200}/>
 
               <span className='mt-1'>Express</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://github.com/")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/github/github-original.svg" width={200} height={200}/>
 
               <span className='mt-1'>Github</span>
@@ -161,58 +157,58 @@ export default function Skill() {
         className="mySwiper"
       >
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://dotnet.microsoft.com/en-us/languages/csharp")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/csharp/csharp-original.svg" width={200} height={200}/>
 
               <span className='mt-1'>C#</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://cplusplus.com/")}  className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/cplusplus/cplusplus-original.svg" width={200} height={200}/>
               <span className='mt-1'>C++</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://www.w3schools.com/c/index.php")}  className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/c/c-original.svg" width={200} height={200}/>
               <span className='mt-1'>C</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://developer.mozilla.org/en-US/docs/Web/HTML")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/html5/html5-original.svg" width={200} height={200}/>
 
               <span className='mt-1'>Html</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://developer.mozilla.org/en-US/docs/Web/CSS")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/css3/css3-original.svg" width={200} height={200}/>
 
               <span className='mt-1'>Css</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://developer.mozilla.org/en-US/docs/Web/JavaScript")}  className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/javascript/javascript-original.svg" width={200} height={200} />
               <span className='mt-1'>Javascript</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://www.python.org/")} className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/python/python-original.svg" width={200} height={200} />
               <span className='mt-1'>Python</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://kotlinlang.org/")}  className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/kotlin/kotlin-original.svg" width={200} height={200} />
               <span className='mt-1'>Kotlin</span>
             </div>
         </SwiperSlide>
         <SwiperSlide>
-            <div className='text-center flex flex-col items-center'>
+            <div onClick={() => router.push("https://www.java.com/en/")}  className='text-center flex flex-col items-center'>
             <Image alt="" src="https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/java/java-original.svg" width={200} height={200} />
               <span className='mt-1'>Java</span>
             </div>
